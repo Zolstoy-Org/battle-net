de1a746f1bba041534083bd776c5afd92e3219e6
Zolstoy <zolstoy.dev@gmail.com>
2025-05-12 14:00:58 +0200

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -9,21 +9,87 @@ pub enum Error {
 }
 
 pub struct Connection {
+    region: Region,
     token: String,
 }
 
+pub enum Region {
+    EU,
+    US,
+    APAC,
+    CN,
+}
+
+impl Region {
+    fn value(&self) -> &str {
+        match *self {
+            Region::EU => "eu",
+            Region::US => "us",
+            Region::APAC => "apac",
+            Region::CN => "cn",
+        }
+    }
+}
+
+pub enum Locale {
+    EnUs,
+    EsMx,
+    PtBr,
+    DeDe,
+    EnGb,
+    EsEs,
+    FrFr,
+    ItIt,
+    RuRu,
+    KoKr,
+    ZhTw,
+    ZhCn,
+}
+
+impl Locale {
+    fn value(&self) -> &str {
+        match *self {
+            Locale::EnUs => "en_US",
+            Locale::EsMx => "es_MX",
+            Locale::PtBr => "pt_BR",
+            Locale::DeDe => "de_DE",
+            Locale::EnGb => "en_GB",
+            Locale::EsEs => "es_ES",
+            Locale::FrFr => "fr_FR",
+            Locale::ItIt => "it_IT",
+            Locale::RuRu => "ru_RU",
+            Locale::KoKr => "ko_KR",
+            Locale::ZhTw => "zn_TW",
+            Locale::ZhCn => "zh_CN",
+        }
+    }
+}
+
+enum Game {
+    WoW,
+}
+
+impl Game {
+    fn value(&self) -> &str {
+        match *self {
+            Game::WoW => "wow",
+        }
+    }
+}
 impl Connection {
-    pub fn new(token: &str) -> Connection {
+    pub fn new(region: Region, token: &str) -> Connection {
         Connection {
             token: token.to_string(),
+            region,
         }
     }
 
-    pub async fn get_nb_auctions(&self) -> u32 {
-        // battle_net_oauth::get_oauth_token("", "", "US")
-
-        0
+    fn get_uri(&self, game: Game, locale: Locale) -> String {
+        format!("https://{region}.api.blizzard.com/data/{game}/connected-realm/106/auctions?namespace=dynamic-{region}&locale={locale}",
+            region = self.region.value(), game = game.value(), locale = locale.value())
     }
+
+    pub async fn get_nb_auctions(&self) -> u32 {}
 }
 
 #[cfg(test)]
@@ -54,25 +120,26 @@ mod tests_battle_net {
         let listener = TcpListener::bind(addr).await?;
 
         // We start a loop to continuously accept incoming connections
-        loop {
-            let (stream, _) = listener.accept().await?;
+        // loop {
+        let (stream, _) = listener.accept().await?;
 
-            // Use an adapter to access something implementing `tokio::io` traits as if they implement
-            // `hyper::rt` IO traits.
-            let io = TokioIo::new(stream);
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
 
-            // Spawn a tokio task to serve multiple connections concurrently
-            tokio::task::spawn(async move {
-                // Finally, we bind the incoming connection to our `hello` service
-                if let Err(err) = http1::Builder::new()
-                    // `service_fn` converts our function in a `Service`
-                    .serve_connection(io, service_fn(hello))
-                    .await
-                {
-                    eprintln!("Error serving connection: {:?}", err);
-                }
-            });
-        }
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
+        // }
+        Ok(())
     }
 
     #[tokio::test]
