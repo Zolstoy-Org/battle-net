8832f2f29cae9dddba8d36478af96756b38a12ac
Zolstoy <zolstoy.dev@gmail.com>
2025-05-13 21:16:43 +0200

    WIP

--- src/instance.rs
+++ src/instance.rs
@@ -1,9 +1,17 @@
+use reqwest::Certificate;
+use rustls::pki_types::pem::PemObject;
+use rustls::pki_types::CertificateDer;
+use rustls::RootCertStore;
+use webpki_roots::TLS_SERVER_ROOTS;
+
 use crate::error::Error;
 use crate::result::Result;
 
 pub struct Instance {
-    region: Region,
     token: String,
+    region: Region,
+    address: String,
+    root_cert_store: RootCertStore,
 }
 
 pub enum Region {
@@ -83,28 +91,41 @@ impl Game {
     }
 }
 
-#[cfg(not(test))]
-const ADDRESS: &str = "api.blizzard.com";
-#[cfg(test)]
-const ADDRESS: &str = "localhost";
-
 impl Instance {
     pub fn new(region: Region, token: &str) -> Instance {
         Instance {
             token: token.to_string(),
+            address: format!("{}.api.blizzard.com", region.value()).to_string(),
             region,
+            root_cert_store: RootCertStore {
+                roots: TLS_SERVER_ROOTS.into(),
+            },
         }
     }
 
+    pub fn set_address(&mut self, address: String) {
+        self.address = address;
+    }
+
+    pub fn add_ca_cert<'a>(&mut self, cert_slice: &'a [u8]) {
+        self.root_cert_store.add_parsable_certificates(
+            CertificateDer::pem_slice_iter(cert_slice).map(|result| result.unwrap()),
+        );
+    }
+
     fn get_uri(&self, game_route: Game, locale: Locale) -> String {
-        format!("https://{region}.{ADDRESS}/data/{game_route_part}/connected-realm/106/auctions?namespace=dynamic-{region}&locale={locale}",
-            region = self.region.value(), game_route_part = game_route.value(), locale = locale.value())
+        format!("https://{address}/data/{game_route_part}/connected-realm/106/auctions?namespace=dynamic-{region}&locale={locale}",
+            address = self.address, region = self.region.value(), game_route_part = game_route.value(), locale = locale.value())
     }
 
     pub async fn get_auctions_by_realm_id(&self, realm_id: u32, locale: Locale) -> Result<u32> {
         let uri = self.get_uri(Game::WoW(WoW::Auctions(realm_id)), locale);
 
-        let client = reqwest::Client::default();
+        let client = reqwest::Client::builder()
+            .use_rustls_tls()
+            .add_root_certificate(Certificate::from_pem(self.root_cert_store.roots.first().unwrap()
+            .build()
+            .unwrap();
 
         let request = reqwest::Request::new(
             reqwest::Method::GET,
